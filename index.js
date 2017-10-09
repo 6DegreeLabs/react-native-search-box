d700581598c2b89998c1f5bb9121e33c66a750bd
Brian DeWeese <brian@briandeweese.org>
2017-10-09 11:55:02 -0700

    use prop-types

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+const PropTypes = require('prop-types');
 import {
     Text,
     TouchableWithoutFeedback,
